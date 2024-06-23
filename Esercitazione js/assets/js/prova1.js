ca3a958bd7e8640ccdb0dfc67c8fc9101dab38c1
METAORI <nico97.mecca@gmail.com>
2024-06-23 21:40:52 +0200

    esercizio n 9

--- Esercitazione js/assets/js/prova1.js
+++ Esercitazione js/assets/js/prova1.js
@@ -95,4 +95,15 @@ for (let i = 0; i < cars.length; i++){
         console.log('Buzz') ;
     }
 }
+/* ESERCIZIO 9
+    Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
+*/
+const numericArray = [6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105, ] ;
 
+let i = 0; 
+while (numericArray[i] !== 32) {
+    console.log(numericArray[i]);
+    i++;
+}
+
+    
